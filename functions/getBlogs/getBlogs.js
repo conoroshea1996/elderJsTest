dfac7f7b7d500c973827e4b861bb3974f53efb8d
conor o shea <conoshea12@gmail.com>
2021-07-06 14:51:50 +0100

    - remove all the build folder

--- functions/getBlogs/getBlogs.js
+++ functions/getBlogs/getBlogs.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const renderElderPage = require('../lib/render-elder-page');
 
+const { Elder, Page } = require('@elderjs/elderjs');
+const elder = new Elder({ context: 'server', configOptions: config });
+
 exports.handler = async (event) => {
   const html = await renderElderPage('/recruiting-blog/', { errorMessage: 'You need to login first' });
+  await elder.bootstrap();
+  const html = new Page({});
   return {
     statusCode: 200,
     body: html,
