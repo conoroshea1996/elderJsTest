aac372496e4fbcd12436b29aa5624a53483e0134
conor o shea <conoshea12@gmail.com>
2021-07-06 12:25:37 +0100

    - add elder js middle ware (test)

--- netlify/functions/getBlogs.js
+++ netlify/functions/getBlogs.js
@@ -6,12 +6,16 @@ const bodyParser = require('body-parser');
 const sirv = require('sirv');
 const serverless = require('serverless-http');
 
-const dev = process.env.NODE_ENV === 'development';
+const { Elder } = require('@elderjs/elderjs');
+const elder = new Elder({ context: 'server' });
 
 const app = express();
 app.use(cors());
 app.use(compression({ level: 6 }));
 app.use(bodyParser.urlencoded({ extended: false }), bodyParser.json());
+
+app.use(elder.server);
+app.use(sirv(elder.settings.distDir, { dev: false }));
 
 app.get('/recruiting-blog', (req, res) => {
   // console.log('params', req.params);
