da8766ffab40540edf9fc9ab73c5094376191a2f
conor o shea <conoshea12@gmail.com>
2021-07-06 11:21:55 +0100

    - test servless app

--- netlify/functions/getBlogs.js
+++ netlify/functions/getBlogs.js
@@ -1,6 +1,22 @@
 require('dotenv').config();
+const polka = require('polka');
+const cors = require('cors');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const sirv = require('sirv');
+const serverless = require('serverless-http');
 
-exports.handler = function (event, context, callback) {
-  console.log(event, 'Event');
-  console.log(context, 'Context');
-};
+const dev = process.env.NODE_ENV === 'development';
+
+const server = polka();
+server.use(cors());
+server.use(compression({ level: 6 }));
+server.use(bodyParser.urlencoded({ extended: false }), bodyParser.json());
+
+app.get('/recruiting-blog', (req, res) => {
+  // console.log('params', req.params);
+  const html = '<h1> Generated On Serve </h1> ';
+  res.html(html);
+});
+
+exports.handler = serverless(server);
