c73e171cdbe60805c436a3c5e46a0c8af85ea0bb
conor o shea <conoshea12@gmail.com>
2021-07-06 11:58:59 +0100

    - use express

--- netlify/functions/getBlogs.js
+++ netlify/functions/getBlogs.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const polka = require('polka');
+const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const bodyParser = require('body-parser');
@@ -8,15 +8,17 @@ const serverless = require('serverless-http');
 
 const dev = process.env.NODE_ENV === 'development';
 
-const server = polka();
-server.use(cors());
-server.use(compression({ level: 6 }));
-server.use(bodyParser.urlencoded({ extended: false }), bodyParser.json());
+const app = express();
+app.use(cors());
+app.use(compression({ level: 6 }));
+app.use(bodyParser.urlencoded({ extended: false }), bodyParser.json());
 
-server.get('/recruiting-blog', (req, res) => {
+app.get('/recruiting-blog', (req, res) => {
   // console.log('params', req.params);
-  const html = '<h1> Generated On Server </h1> ';
+  const html = '<h1> Generated On Server </h1>';
+
+  console.log(html);
   res.html(html);
 });
 
-exports.handler = serverless(server);
+exports.handler = serverless(app);
