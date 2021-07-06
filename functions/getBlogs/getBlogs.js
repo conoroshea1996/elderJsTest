8cce58832996c75a35a850f17a04243814f90089
conor o shea <conoshea12@gmail.com>
2021-07-06 19:01:27 +0100

    -test rendering blog

--- functions/getBlogs/getBlogs.js
+++ functions/getBlogs/getBlogs.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 const renderElderPage = require('../lib/render-elder-page');
 
 exports.handler = async (event) => {
-  console.log(renderElderPage);
-  // const html = await renderElderPage('/recruiting-blog/', { errorMessage: 'You need to login first' });
+  const html = await renderElderPage('/recruiting-blog/');
 
-  const html = 'h1';
   return {
     statusCode: 200,
     body: html,
