6dd88376304d10ee0a33d02ea0999e8a48c6d407
conor o shea <conoshea12@gmail.com>
2021-07-07 15:39:02 +0100

    - change priority level so data hook runs first

--- netlify/functions/getBlogs/render-elder-page.js
+++ netlify/functions/getBlogs/render-elder-page.js
@@ -3,7 +3,7 @@ const config = require('./elder.config');
 
 const elder = new Elder({ context: 'server', ...config });
 
-module.exports = async function renderElderPage(permalink, data = {}) {
+module.exports = async function renderElderPage(permalink, data) {
   await elder.bootstrap();
   const request = elder.serverLookupObject['/recruiting-blog/'];
   console.log(request, 'Request');
@@ -17,8 +17,9 @@ module.exports = async function renderElderPage(permalink, data = {}) {
     hook: 'data',
     name: 'addData',
     description: 'Adds custom data to data object',
-    priority: 50,
+    priority: 1,
     run: (opts) => {
+      console.log(opts);
       return {
         data: { ...opts.data, ...data },
       };
@@ -28,12 +29,10 @@ module.exports = async function renderElderPage(permalink, data = {}) {
   elder.hooks.push(dataHook);
   // console.log(elder.hooks, 'ELDER HOOKS');
 
-  console.log(data, 'DATA FOR PAGE');
   const page = new Page({
     ...elder,
     request,
     route,
-    data: { ...data },
   });
 
   return await page.html();
