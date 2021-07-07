364d5cd8c9885cd9420752a25b519e077c8ca274
conor o shea <conoshea12@gmail.com>
2021-07-07 11:59:20 +0100

    - fix root directory

--- elder.config.prod.js
+++ elder.config.prod.js
@@ -7,9 +7,9 @@ console.log(markdownPlugin, 'Mark Down plugin');
 module.exports = {
   origin: '', // TODO: update this.
   lang: 'en',
-  srcDir: 'getBlogs/src',
-  distDir: 'getBlogs/public',
-  rootDir: path.resolve(__dirname),
+  srcDir: 'src',
+  distDir: 'public',
+  rootDir: path.resolve(__dirname, '../getBlogs'),
   build: {},
   prefix: '', // If you want your site to be built within a sub folder within your `distDir` you can use this.
   server: {},
