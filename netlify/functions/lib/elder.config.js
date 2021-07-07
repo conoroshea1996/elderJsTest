afb2231976a51a969ba591feb285e81c5e738da1
conor o shea <conoshea12@gmail.com>
2021-07-07 11:12:19 +0100

    - move neltify functions back to netlify functions folder

--- netlify/functions/lib/elder.config.js
+++ netlify/functions/lib/elder.config.js
@@ -1,19 +1,16 @@
 require('dotenv').config();
 const path = require('path');
+let x = require('@elderjs/plugin-markdown');
 
 module.exports = {
   origin: '', // TODO: update this.
   lang: 'en',
-  srcDir: 'src',
-  distDir: 'public',
   rootDir: path.resolve(__dirname, '../getBlogs'),
+  distDir: 'public',
+  srcDir: 'src',
   build: {},
   prefix: '', // If you want your site to be built within a sub folder within your `distDir` you can use this.
   server: {},
-  props: {
-    hydration: 'hybrid',
-    compress: false,
-  },
   debug: {
     stacks: false, // output details of the stack consolidation process.
     hooks: false, // outputs the details of each hook as they are run.
@@ -24,14 +21,12 @@ module.exports = {
   hooks: {
     // disable: ['elderWriteHtmlFileToPublic'], // this is used to disable internal hooks. Uncomment this hook to disabled writing your files during build.
   },
+
   plugins: {
     '@elderjs/plugin-markdown': {
       routes: ['blog', 'textPages', 'FRblog', 'customer'],
     },
-    // '@elderjs/plugin-seo-check': {
-    //   display: ['errors'], // If the errors are too verbose remove 'warnings'
-    //   //writeLocation: './report.json', // if you want to write a report of errors
-    // },
   },
+
   shortcodes: { closePattern: '}}', openPattern: '{{' },
 };
