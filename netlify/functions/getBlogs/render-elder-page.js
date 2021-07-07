f88848706fde361e65d08314cda92d9216732830
conor o shea <conoshea12@gmail.com>
2021-07-07 19:18:46 +0100

    - add logs for debugging

--- netlify/functions/getBlogs/render-elder-page.js
+++ netlify/functions/getBlogs/render-elder-page.js
@@ -16,12 +16,14 @@ module.exports = async function renderElderPage(permalink, extraData) {
     description: 'Use this hook to add a key to the "data" object on the "home" route. ',
     priority: 50,
     run: async ({ request, data }) => {
+      console.log(data, 'DATA');
       let allBlogs = data.markdown.blog;
 
       if (extraData.search) {
         allBlogs = allBlogs.filter((p) => p.frontmatter.title.toLowerCase().includes(extraData.search.toLowerCase()));
       }
 
+      console.log(allBlogs, 'ALL BLOGS SEARCH ');
       return {
         data: {
           ...data,
