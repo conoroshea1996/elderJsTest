66d0acc657654cf81c9fdaddbd545fe0572041c9
conor o shea <conoshea12@gmail.com>
2021-07-07 14:38:59 +0100

    fix path to render page

--- netlify/functions/getBlogs/getBlogs.js
+++ netlify/functions/getBlogs/getBlogs.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const renderElderPage = require('../lib/render-elder-page');
+const renderElderPage = require('./render-elder-page');
 
 exports.handler = async (event) => {
   // we need to populate the data here manually the markdown hook doesn't seem to populate this data for us
