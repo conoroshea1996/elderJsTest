0a7f27ee5da670d004a3b7074ba54cc7d36f47bd
conor o shea <conoshea12@gmail.com>
2021-07-07 12:13:22 +0100

    - change root directory to base of function

--- elder.config.prod.js
+++ elder.config.prod.js
@@ -7,9 +7,9 @@ console.log(markdownPlugin, 'Mark Down plugin');
 module.exports = {
   origin: '', // TODO: update this.
   lang: 'en',
-  srcDir: 'src',
-  distDir: 'public',
-  rootDir: path.resolve(__dirname, '../getBlogs'),
+  srcDir: 'getBlogs/src',
+  distDir: 'getBlogs/public',
+  rootDir: path.resolve(__dirname, '../'),
   build: {},
   prefix: '', // If you want your site to be built within a sub folder within your `distDir` you can use this.
   server: {},
