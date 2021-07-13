7ef6ee4cb1570c0954a44180e336b0e432a8d686
conor o shea <conoshea12@gmail.com>
2021-07-13 11:06:40 +0100

    - add form submission function

---
+++ netlify/functions/submission-created/submission-created.js
@@ -0,0 +1,49 @@
+const fetch = require('node-fetch');
+const convert_kit_api_key = 'xGUlpDvdVvVXb1UwYnkuug';
+const blogPostNewsLetter = {
+  //newsletter
+  formName: 'blogNewsletter',
+  // convert kit form id
+  formId: 2123005,
+};
+const postData = async (url, body) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+  return response.json();
+};
+// https://api.convertkit.com/v3/forms/<form_id>/subscribe
+exports.handler = async function (event, context) {
+  console.log(event);
+
+  let data = new URLSearchParams(event.body);
+  const formName = data.get('form-name');
+  let email = data.get('email');
+
+  if (formName == blogPostNewsLetter.formName) {
+    email = email.toLowerCase();
+    const bodyToSend = {
+      api_key: convert_kit_api_key,
+      email,
+    };
+    const body = JSON.stringify(bodyToSend);
+    console.log(bodyToSend, 'Request Body');
+    try {
+      let res = await postData(`https://api.convertkit.com/v3/forms/${blogPostNewsLetter.formId}/subscribe`, body);
+      console.log(res, 'SUCCESS');
+      return {
+        statusCode: 200,
+        body: '<h1> *** Thanks Page ***</h1>',
+      };
+    } catch (error) {
+      console.log(error, 'ERROR');
+      return {
+        statusCode: 404,
+      };
+    }
+  }
+};
