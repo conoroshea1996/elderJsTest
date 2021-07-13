25dccfc535fa05d9f503550036356f78c35e844e
conor o shea <conoshea12@gmail.com>
2021-07-13 10:22:15 +0100

    - use sitemap plugin for english - use sitemap generator for french sitemap

--- test.js
+++ test.js
@@ -1,20 +1,6 @@
 const SitemapGenerator = require('sitemap-generator');
 
 // create generator
-const generator = SitemapGenerator('https://youthful-mccarthy-ab0370.netlify.app/', {
-  stripQuerystring: false,
-  filepath: './public/sitemap.xml',
-  ignore: (url) => {
-    return url.includes('fr/');
-  },
-});
-
-// start the crawler
-generator.start();
-
-generator.on('done', () => {
-  console.log('ENGLISH DONE');
-});
 
 // create generator
 const Frenchgenerator = SitemapGenerator('https://youthful-mccarthy-ab0370.netlify.app/fr', {
