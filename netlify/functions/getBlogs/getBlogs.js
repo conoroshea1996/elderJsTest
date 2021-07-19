1a99c264da60aa8462637b6b2f48ebb281256cad
conor o shea <conoshea12@gmail.com>
2021-07-19 17:45:15 +0100

    - get page number

--- netlify/functions/getBlogs/getBlogs.js
+++ netlify/functions/getBlogs/getBlogs.js
@@ -2,7 +2,19 @@ const renderElderPage = require('./render-elder-page');
 
 exports.handler = async (event) => {
   console.log(event, 'EVENT');
-  const html = await renderElderPage('/recruiting-blog/', { search: event.queryStringParameters['search'] });
+
+  const path = event.path.split('/');
+  let page;
+  if (path[2]) {
+    page = Number.parseInt(path[2]);
+  } else {
+    page = 0;
+  }
+
+  const html = await renderElderPage('/recruiting-blog/', {
+    search: event.queryStringParameters['search'],
+    page: page,
+  });
 
   return {
     statusCode: 200,
--- netlify/functions/getBlogs/render-elder-page.js
+++ netlify/functions/getBlogs/render-elder-page.js
@@ -10,7 +10,10 @@ module.exports = async function renderElderPage(permalink, extraData) {
   // console.log('elder locations', elder.settings.locations);
 
   const route = elder.routes['recruitingBlog'];
-  
+
+  const postPerPage = 10;
+  let allPagesCount;
+
   const dataHook = {
     hook: 'data',
     name: 'addSomethingToData',
@@ -32,6 +35,7 @@ module.exports = async function renderElderPage(permalink, extraData) {
         blogs = blogs.filter((p) => p.frontmatter.title.toLowerCase().includes(extraData.search.toLowerCase()));
       }
 
+      allPagesCount = Math.floor(blogs.length / postPerPage);
       return {
         data: {
           ...data,
@@ -45,13 +49,14 @@ module.exports = async function renderElderPage(permalink, extraData) {
 
   request = {
     slug: 'recruiting-blog/',
-    postStart: 10,
-    postEnd: 20,
-    lastPage: 29,
-    hasPrevious: false,
-    hasNext: true,
-    page: 2,
-    nextPage: { slug: `recruiting-blog/2?search=${extraData.search}` },
+    postStart: extraData.page * postPerPage,
+    postEnd: extraData.page * postPerPage + postPerPage,
+    isLastPage: extraData.page === allPagesCount - 1,
+    hasPrevious: extraData.page > 0,
+    hasNext: extraData.page !== allPagesCount - 1,
+    page: extraData.page,
+    previousPage: { slug: `recruiting-blog/${extraData.page - 1}?search=${extraData.search}` },
+    nextPage: { slug: `recruiting-blog/${extraData.page + 1}?search=${extraData.search}` },
     template: 'recruitingBlog',
     route: 'recruitingBlog',
     type: 'server',
@@ -63,8 +68,6 @@ module.exports = async function renderElderPage(permalink, extraData) {
     request,
     route,
   });
-
-  console.log(page, 'Page');
 
   return await page.html();
 };
