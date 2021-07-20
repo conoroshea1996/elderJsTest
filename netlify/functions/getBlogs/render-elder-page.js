533827acf41b9a98e5c4ab55e1e9aa01c9037aae
conor o shea <conoshea12@gmail.com>
2021-07-20 11:38:08 +0100

    fix for check to see if blog results has next

--- netlify/functions/getBlogs/render-elder-page.js
+++ netlify/functions/getBlogs/render-elder-page.js
@@ -68,7 +68,7 @@ module.exports = async function renderElderPage(permalink, extraData, isFrenchBl
     postEnd: extraData.page * postPerPage + postPerPage,
     isLastPage: extraData.page === allPagesCount - 1,
     hasPrevious: extraData.page > 0,
-    hasNext: extraData.page !== allPagesCount - 1,
+    hasNext: extraData.page < allPagesCount - 1,
     page: extraData.page,
     previousPage: { slug: `${slug}/${extraData.page - 1}?search=${extraData.search}` },
     nextPage: { slug: `${slug}/${extraData.page === 0 ? 2 : extraData.page + 1}?search=${extraData.search}` },
