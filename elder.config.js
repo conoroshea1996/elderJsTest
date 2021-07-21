f41af8361cae9d250ca64a680a351b2e69aadb40
conor o shea <conoshea12@gmail.com>
2021-07-21 11:48:19 +0100

    - git lfs all assets - add git attributes - dont use image plugin

--- elder.config.js
+++ elder.config.js
@@ -31,28 +31,28 @@ module.exports = {
       port: 8080,
       reload: false, // if you are having issues with reloading not working, change to true.
     },
-    '@elderjs/plugin-images': {
-      // debug: false,
-      // cssString: false,
-      // //comonent out when deploying to netlify so it doesn't make the images again takes ages
-      folders: [
-        {
-          src: 'assets/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
-          output: 'images/', // where files should be put within the distDir defined in your elder.config.js.
-        },
-        {
-          src: 'assets/fr/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
-          output: 'fr/images/', // where files should be put within the distDir defined in your elder.config.js.
-        },
-        {
-          src: 'assets/customers/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
-          output: 'customers/images/', // where files should be put within the distDir defined in your elder.config.js.
-        },
-      ],
-      imageManifest: '/images/ejs-image-manifest.json',
-      addVanillaLazy: true, // if you want to disable the lazyload plugin and add your own.
-      widths: [1280, 768, 576, 400], // Sizes the images will be resized to.
-    },
+    // '@elderjs/plugin-images': {
+    //   // debug: false,
+    //   // cssString: false,
+    //   // //comonent out when deploying to netlify so it doesn't make the images again takes ages
+    //   folders: [
+    //     {
+    //       src: 'assets/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
+    //       output: 'images/', // where files should be put within the distDir defined in your elder.config.js.
+    //     },
+    //     {
+    //       src: 'assets/fr/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
+    //       output: 'fr/images/', // where files should be put within the distDir defined in your elder.config.js.
+    //     },
+    //     {
+    //       src: 'assets/customers/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
+    //       output: 'customers/images/', // where files should be put within the distDir defined in your elder.config.js.
+    //     },
+    //   ],
+    //   imageManifest: '/images/ejs-image-manifest.json',
+    //   addVanillaLazy: true, // if you want to disable the lazyload plugin and add your own.
+    //   widths: [1280, 768, 576, 400], // Sizes the images will be resized to.
+    // },
     '@elderjs/plugin-sitemap': {
       origin: 'https://youthful-mccarthy-ab0370.netlify.app',
       exclude: [], // an array of permalinks or permalink prefixes. So you can do ['500'] and it will match /500**
